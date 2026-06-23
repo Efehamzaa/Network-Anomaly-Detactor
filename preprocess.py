08d8d8814aae5fe73bd7fad848e4c32a8198a7f7
Efehamzaa <bayavefehamza@gmail.com>
2026-06-23 17:49:10 -0400

    feat: 3. Gün - Ham ağ verisini ML formatına çeviren Data Preprocessing mimarisi kuruldu

---
+++ preprocess.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+print("Ham Veri Yukleniyor ...")
+df=pd.read_csv("normal_trafik.csv")
+
+print("Kaynak ve Hedef IP adresleri tablodan temizleniyor ...")
+df=df.drop(['Kaynak_IP' , 'Hedef_IP'] , axis=1)
+
+print("TCP Bayrakları matematiksel matrise donusturuluyor...")
+df=pd.get_dummies(df , columns=['TCP_Bayrak'] , drop_first=False , dtype=int)
+
+print("sayisal veriler standartlastiriliyor ...")
+scaler=StandardScaler()
+olceklenecek_sutunlar= ['Zaman' , 'Protokol' , 'Hedef_Port' ,'Boyut']
+df[olceklenecek_sutunlar]= scaler.fit_transform(df[olceklenecek_sutunlar])
+
+print("\n --- İslenmis veri Seti (ilk 5 satir) ---")
+print(df.head())
+
+df.to_csv("islenmis_trafik.csv" , index=False)
+print("\n BASARİLİ: Veri seti Makine Ogrenmesi (ML) icin hazirlandi ve 'islenmis_trafik.csv' olarak kaydedildi!")
